e87471b833c135aba4c9139df7a2abf3a7eb4c29
Rosie Wu <b06902026@ntu.edu.tw>
2020-01-12 05:37:48 +0000

    Add messages into database

--- chat_room/views.py
+++ chat_room/views.py
@@ -1,10 +1,12 @@
 import json
+from datetime import datetime
 from rest_framework import generics
 from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.utils.safestring import mark_safe
+from chat_room.models import Message
 
 def index(request):
     return render(request, 'index.html')
@@ -28,10 +30,22 @@ def logout_view(request):
     return redirect('/')
 
 def messages(request):
-    return HttpResponse('messages')
+    if request.method == 'POST':
+        data = request.POST
+        message = Message(room_id=data['room_id'],
+                          sender=data['sender'],
+                          time=datetime.now(),
+                          text=data['text'])
+        message.save()
+        return HttpResponse('success')
+    return HttpResponse('failed')
 
 def room(request, room_name):
+    texts = []
+    for message in Message.objects.filter(room_id=room_name):
+        texts.append(message.text)
     return render(request, 'room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'texts': mark_safe(json.dumps(texts)),
     })
 
